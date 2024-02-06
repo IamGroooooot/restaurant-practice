5a415de583cbdd078e03e00f6792dc8af2b2c8c1
Juhyeong Ko <dury.ko@gmail.com>
2024-02-07 01:41:52 +0900

    Feat: OOP style web server

--- Restaurant.RestApi/Program.cs
+++ Restaurant.RestApi/Program.cs
@@ -1,5 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace Restaurant.RestApi;
 
-app.MapGet("/", () => "Hello World!");
-app.Run();
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
---
+++ Restaurant.RestApi/Startup.cs
@@ -0,0 +1,25 @@
+namespace Restaurant.RestApi;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+public sealed class Startup
+{
+    public static void Configure(
+        IApplicationBuilder app,
+        IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+        
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        });
+    }
+}
