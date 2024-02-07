936174e6b4899f5514638e557a5bda9bdd8997ae
Juhyeong Ko <dury.ko@gmail.com>
2024-02-07 17:02:44 +0900

    Lint: use filescope namespace

--- Restaurant.RestApi.tests/HomeTests.cs
+++ Restaurant.RestApi.tests/HomeTests.cs
@@ -1,17 +1,16 @@
 using Restaurant.RestApi.Tests;
 
-namespace Restaurant.RestApi.tests
+namespace Restaurant.RestApi.tests;
+
+public sealed class HomeTests
 {
-    public sealed class HomeTests
+    [Test]
+    public async Task ShouldReturnHelloWorld()
     {
-        [Test]
-        public async Task ShouldReturnHelloWorld()
-        {
-            var app = new WebApplicationFactory<Startup>();
-            var client = app.CreateClient();
-            var response = await client.GetAsync("/");
-            var content = await response.Content.ReadAsStringAsync();
-            Assert.That(content, Is.EqualTo("Hello World!"));
-        }
+        var app = new WebApplicationFactory<Startup>();
+        var client = app.CreateClient();
+        var response = await client.GetAsync("/");
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.That(content, Is.EqualTo("Hello World!"));
     }
 }
--- Restaurant.RestApi.tests/WebApplicationFactory.cs
+++ Restaurant.RestApi.tests/WebApplicationFactory.cs
@@ -2,25 +2,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 
-namespace Restaurant.RestApi.Tests
+namespace Restaurant.RestApi.Tests;
+
+public class WebApplicationFactory<TStartup> : Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactory<TStartup> where TStartup : class
 {
-    public class WebApplicationFactory<TStartup> : Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactory<TStartup> where TStartup : class
+    protected override IWebHostBuilder CreateWebHostBuilder()
     {
-        protected override IWebHostBuilder CreateWebHostBuilder()
-        {
-            return new WebHostBuilder().UseStartup<TStartup>();
-        }
+        return new WebHostBuilder().UseStartup<TStartup>();
+    }
 
-        protected override TestServer CreateServer(IWebHostBuilder builder)
-        {
-            return new TestServer(builder);
-        }
+    protected override TestServer CreateServer(IWebHostBuilder builder)
+    {
+        return new TestServer(builder);
+    }
 
-        public new HttpClient CreateClient()
-        {
-            var client = CreateClient(new WebApplicationFactoryClientOptions());
-            client.BaseAddress = new Uri("http://localhost:5000");
-            return client;
-        }
+    public new HttpClient CreateClient()
+    {
+        var client = CreateClient(new WebApplicationFactoryClientOptions());
+        client.BaseAddress = new Uri("http://localhost:5000");
+        return client;
     }
 }
