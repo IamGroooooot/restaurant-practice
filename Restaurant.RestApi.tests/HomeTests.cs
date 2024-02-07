06c855fe4f750aa516442379cd6c705430d6789c
Juhyeong Ko <dury.ko@gmail.com>
2024-02-07 16:55:42 +0900

    Test: Home Return HelloWorld

---
+++ Restaurant.RestApi.tests/HomeTests.cs
@@ -0,0 +1,17 @@
+using Restaurant.RestApi.Tests;
+
+namespace Restaurant.RestApi.tests
+{
+    public sealed class HomeTests
+    {
+        [Test]
+        public async Task ShouldReturnHelloWorld()
+        {
+            var app = new WebApplicationFactory<Startup>();
+            var client = app.CreateClient();
+            var response = await client.GetAsync("/");
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.That(content, Is.EqualTo("Hello World!"));
+        }
+    }
+}
---
+++ Restaurant.RestApi.tests/WebApplicationFactory.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Restaurant.RestApi.Tests
+{
+    public class WebApplicationFactory<TStartup> : Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            return new WebHostBuilder().UseStartup<TStartup>();
+        }
+
+        protected override TestServer CreateServer(IWebHostBuilder builder)
+        {
+            return new TestServer(builder);
+        }
+
+        public new HttpClient CreateClient()
+        {
+            var client = CreateClient(new WebApplicationFactoryClientOptions());
+            client.BaseAddress = new Uri("http://localhost:5000");
+            return client;
+        }
+    }
+}
